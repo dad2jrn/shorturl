901a1cd14cfa535c99a357af87952d25ec27d1d0
dad2jrn <dad2jrn@gmail.com>
2023-10-02 10:48:54 -0400

    .env and cache settings

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 # src/config.py
 
 from pydantic import BaseSettings
+from functools import lru_cache
 
 class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./shortner.db"
 
+@lru_cache #cache the data to memory
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
