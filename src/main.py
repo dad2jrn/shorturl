63fbf8225e8f70e2a97ffbfc036307c93da81fc2
dad2jrn <dad2jrn@gmail.com>
2023-10-02 10:59:56 -0400

    schema and root

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
 # src/main.py
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "Welcome to the URL shortner API..."
---
+++ src/schema.py
@@ -0,0 +1,18 @@
+# src/schema.py
+
+from pydantic import BaseModel
+
+class URLBase(BaseModel):
+    target_url: str
+
+class URL(URLBase):
+    is_active: bool
+    clicks: int
+
+    class Config:
+        orm_model = True
+
+class URLInfo(URL):
+    url: str
+    admin_url: str
+
